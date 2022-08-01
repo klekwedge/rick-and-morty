5cb685a6a17d5afc6098e2f628a41f894b40de6a
klekkwedge <antonov.andrey.10101@gmail.com>
2022-08-01 16:53:39 +0300

    fix: change ErrorMessage component

--- rick-and-morty/src/components/ErrorMessage/ErrorMessage.js
+++ rick-and-morty/src/components/ErrorMessage/ErrorMessage.js
@@ -1,7 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import "./ErrorMessage.scss";
 
 const ErrorMessage = () => {
-  return <h3>!!!!!!!!!!!!!!!</h3>;
+  return (
+    <div id="container">
+      <div id="error-box">
+        <div className="face">
+          <div className="eye"></div>
+          <div className="eye right"></div>
+          <div className="mouth sad"></div>
+        </div>
+        <div className="shadow scale"></div>
+        <div className="message">
+          <h3 className="alert">Error!</h3>
+          <p>Oh no, something went wrong.</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorMessage;
