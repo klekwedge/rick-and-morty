18506f5104de48396f52d5d71fee46a7584152e6
klekkwedge <antonov.andrey.10101@gmail.com>
2022-08-03 10:33:14 +0300

    feat: add RandomChar component

---
+++ src/components/RandomChar/RandomChar.js
@@ -0,0 +1,111 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Flex, Heading, Image, Button } from "@chakra-ui/react";
+import RickAndMortyService from "../../services/RickAndMortyService";
+import Spinner from "../Spinner/Spinner";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+
+const CharacterTemplate = ({ character, onRequest }) => {
+  return (
+    <Flex
+      justifyContent="space-between"
+      backgroundColor="#3C3E44"
+      p="20px"
+      color="white"
+      borderRadius="10px"
+    >
+      <Flex gap="40px">
+        <Image
+          src={character.image}
+          alt={character.name}
+          borderRadius="5px"
+          title={character.name}
+          maxWidth="200px"
+          maxHeight="200px"
+        />
+        <Flex flexDirection="column" gap="5px">
+          <Heading as="h3" fontWeight="500" fontSize="18px">
+            Name: {character.name}
+          </Heading>
+          <h3>Gender: {character.gender}</h3>
+          <h3>Species: {character.species}</h3>
+          <h3>Status: {character.status}</h3>
+          <h3>Location: {character.location.name}</h3>
+          <Heading as="h3" fontWeight="400" fontSize="16px" mb="10px">
+            Origin: {character.origin.name}
+          </Heading>
+          <Heading as="h3" fontWeight="400" fontSize="16px">
+            Episodes: {character.episode.length}
+          </Heading>
+        </Flex>
+      </Flex>
+      <Flex gap="20px">
+        <Button
+          alignSelf="flex-end"
+          border="2px solid #FF9800"
+          background="inherit"
+          transition="all 0.4s ease"
+          _hover={{ background: "#FF9800" }}
+        >
+          <Link to={`/characters/${character.id}`}>HomePage</Link>
+        </Button>
+        <Button
+          alignSelf="flex-end"
+          border="2px solid #FF9800"
+          background="inherit"
+          transition="all 0.4s ease"
+          _hover={{ background: "#FF9800" }}
+          onClick={onRequest}
+        >
+          Random char
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
+
+const RandomChar = () => {
+  const [currentChar, setCurrentChar] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const rickAndMortyService = new RickAndMortyService();
+
+  useEffect(() => {
+    onRequest();
+  }, []);
+
+  const onRequest = () => {
+    const charId = Math.floor(Math.random() * (826 - 1) + 1);
+    rickAndMortyService
+      .getCharacter(charId)
+      .then(onCharacterLoaded)
+      .catch(onError);
+  };
+
+  const onCharacterLoaded = (data) => {
+    setCurrentChar(data);
+    setLoading(false);
+  };
+
+  const onError = () => {
+    setLoading(false);
+    setError(true);
+  };
+
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const spinner = loading ? <Spinner /> : null;
+  const content = !(error || loading) ? (
+    <CharacterTemplate character={currentChar} onRequest={onRequest} />
+  ) : null;
+
+  return (
+    <>
+      {errorMessage}
+      {spinner}
+      {content}
+    </>
+  );
+};
+
+export default RandomChar;
--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { Flex } from "@chakra-ui/react";
 import CharList from "../CharList.js/CharList";
 import CharInfo from "../CharInfo.js/CharInfo";
 import ErrorFuse from "../ErrorFuse/ErrorFuse";
+import RandomChar from "../RandomChar/RandomChar";
 
 function MainPage() {
   const [selectedChar, setChar] = useState(null);
@@ -11,14 +13,19 @@ function MainPage() {
   };
 
   return (
-    <>
+    <Flex flexDirection="column" gap='40px'>
       <ErrorFuse>
-        <CharList onCharSelected={onCharSelected} />
+        <RandomChar />
       </ErrorFuse>
-      <ErrorFuse>
-        <CharInfo charId={selectedChar} />
-      </ErrorFuse>
-    </>
+      <Flex gap="50px">
+        <ErrorFuse>
+          <CharList onCharSelected={onCharSelected} />
+        </ErrorFuse>
+        <ErrorFuse>
+          <CharInfo charId={selectedChar} />
+        </ErrorFuse>
+      </Flex>
+    </Flex>
   );
 }
 
