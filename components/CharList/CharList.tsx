1cc90b868153166307406bf36ce0532b428a143d
Andrey <antonov.andrey.10101@gmail.com>
2022-11-20 21:03:03 +0300

    feat: add character interface

--- components/CharList/CharList.tsx
+++ components/CharList/CharList.tsx
@@ -1,48 +1,47 @@
 /* eslint-disable no-return-assign */
-import React, { useState, useEffect, useRef } from 'react';
-import {
-  Flex, List, ListItem, Image, Heading, Button,
-} from '@chakra-ui/react';
-import RickAndMortyService from '../../services/RickAndMortyService';
-import Spinner from '../Spinner/Spinner';
-import ErrorMessage from '../ErrorMessage/ErrorMessage';
-import LikeButton from '../LikeButton/LikeButton';
+import React, { useState, useEffect, useRef } from "react";
+import { Flex, List, ListItem, Image, Heading, Button } from "@chakra-ui/react";
+import RickAndMortyService from "../../services/RickAndMortyService";
+import Spinner from "../Spinner/Spinner";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+// import LikeButton from '../LikeButton/LikeButton';
 // import CharSearchForm from '../CharSearchForm/CharSearchForm';
-import './CharList.module.scss';
+import "./CharList.module.scss";
+import { ICharacter } from "../../types/character.types";
 
 function CharList({ onCharSelected, onCharFavorite, onOpen }: any) {
-  const [charList, setCharList] = useState([]);
+  const [charList, setCharList] = useState<ICharacter[]>([]);
   const [currentCharPage, setCurrentCharPage] = useState(1);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
-  const charRefs = useRef([]);
+  // const charRefs = useRef<HTMLLIElement[]>([]);
 
-  const focusOnItem = (id) => {
-    charRefs.current.forEach((myRef) => myRef.classList.remove('_active'));
-    charRefs.current[id].classList.add('_active');
-    charRefs.current[id].focus();
-  };
+  // const focusOnItem = (id: number) => {
+  //   charRefs.current.forEach((myRef) => myRef.classList.remove('_active'));
+  //   charRefs.current[id].classList.add('_active');
+  //   charRefs.current[id].focus();
+  // };
 
-  const addCharToFavorite = (e, item, index) => {
-    if (
-      e.target.tagName === 'svg'
-      || e.target.tagName === 'INPUT'
-      || e.target.tagName === 'circle'
-      || e.target.tagName === 'path'
-    ) {
-      const option = e.currentTarget.childNodes[2].childNodes[0].checked ? 'add' : 'delete';
-      onCharFavorite(item, option);
-    } else {
-      focusOnItem(index);
-      onCharSelected(item);
-      onOpen();
-    }
-  };
+  // const addCharToFavorite = (e, item, index) => {
+  //   if (
+  //     e.target.tagName === 'svg'
+  //     || e.target.tagName === 'INPUT'
+  //     || e.target.tagName === 'circle'
+  //     || e.target.tagName === 'path'
+  //   ) {
+  //     const option = e.currentTarget.childNodes[2].childNodes[0].checked ? 'add' : 'delete';
+  //     onCharFavorite(item, option);
+  //   } else {
+  //     focusOnItem(index);
+  //     onCharSelected(item);
+  //     onOpen();
+  //   }
+  // };
 
   const rickAndMortyService = new RickAndMortyService();
 
-  const onCharListLoaded = (newCharList) => {
+  const onCharListLoaded = (newCharList: ICharacter[]) => {
     setLoading(false);
     setCharList([...charList, ...newCharList]);
   };
@@ -52,8 +51,11 @@ function CharList({ onCharSelected, onCharFavorite, onOpen }: any) {
     setLoading(false);
   };
 
-  const onRequest = (currentPage) => {
-    rickAndMortyService.getAllCharacters(currentPage).then(onCharListLoaded).catch(onError);
+  const onRequest = (currentPage: number) => {
+    rickAndMortyService
+      .getAllCharacters(currentPage)
+      .then(onCharListLoaded)
+      .catch(onError);
   };
 
   useEffect(() => {
@@ -65,50 +67,50 @@ function CharList({ onCharSelected, onCharFavorite, onOpen }: any) {
   const errorMessage = error ? <ErrorMessage /> : null;
   const content = !(loading || error)
     ? charList.map((item, i) => (
-      <ListItem
-        // tabIndex="0"
-        key={item.id}
-        ref={(el) => (charRefs.current[i] = el)}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        gap="8px"
-        background="#3C3E44"
-        color="white"
-        flex="1 1 20%"
-        pb="15px"
-        overflow="hidden"
-        cursor="pointer"
-        position="relative"
-        borderRadius="5px"
-        onClick={(e) => {
-          addCharToFavorite(e, item, i, item.id);
-        }}
-        onKeyPress={(e) => {
-          if (e.key === ' ' || e.key === 'Enter') {
-            onCharSelected(item);
-            focusOnItem(i);
-          }
-        }}
-        transition="all .3s ease-in-out"
-        _hover={{ transform: 'scale(1.05)' }}
-      >
-        <Image alt={`${item.name} image`} src={item.image} />
-        <Heading as="h2" fontSize="20px" textAlign="center">
-          {item.name}
-        </Heading>
-        <Button
-          position="absolute"
-          top="-7px"
-          right="-20px"
-          background="transparent"
-          _active={{ bacground: 'transparent' }}
-          _hover={{ bacground: 'transparent' }}
+        <ListItem
+          // tabIndex="0"
+          key={item.id}
+          // ref={(el) => (charRefs.current[i] = el)}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          gap="8px"
+          background="#3C3E44"
+          color="white"
+          flex="1 1 20%"
+          pb="15px"
+          overflow="hidden"
+          cursor="pointer"
+          position="relative"
+          borderRadius="5px"
+          // onClick={(e) => {
+          //   addCharToFavorite(e, item, i, item.id);
+          // }}
+          // onKeyPress={(e) => {
+          //   if (e.key === ' ' || e.key === 'Enter') {
+          //     onCharSelected(item);
+          //     focusOnItem(i);
+          //   }
+          // }}
+          transition="all .3s ease-in-out"
+          _hover={{ transform: "scale(1.05)" }}
         >
-          <LikeButton itemId={item.id} />
-        </Button>
-      </ListItem>
-    ))
+          <Image alt={`${item.name} image`} src={item.image} />
+          <Heading as="h2" fontSize="20px" textAlign="center">
+            {item.name}
+          </Heading>
+          <Button
+            position="absolute"
+            top="-7px"
+            right="-20px"
+            background="transparent"
+            _active={{ bacground: "transparent" }}
+            _hover={{ bacground: "transparent" }}
+          >
+            {/* <LikeButton itemId={item.id} /> */}
+          </Button>
+        </ListItem>
+      ))
     : null;
 
   return (
@@ -124,7 +126,7 @@ function CharList({ onCharSelected, onCharFavorite, onOpen }: any) {
         {content}
       </List>
       <Button
-        display={currentCharPage <= 42 ? 'block' : 'none'}
+        display={currentCharPage <= 42 ? "block" : "none"}
         onClick={() => {
           setCurrentCharPage(() => currentCharPage + 1);
           onRequest(currentCharPage);
@@ -133,7 +135,7 @@ function CharList({ onCharSelected, onCharFavorite, onOpen }: any) {
         background="#3C3E44"
         color="white"
         maxWidth="200px"
-        _hover={{ background: '#FF9800' }}
+        _hover={{ background: "#FF9800" }}
       >
         Load more
       </Button>
