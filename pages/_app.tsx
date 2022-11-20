baaf1e999a415e2d5a4b7c9483e3a64353d26ba1
Andrey <antonov.andrey.10101@gmail.com>
2022-11-20 17:19:21 +0300

    fix: resolve conflicts on rendering of _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,23 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Flex } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import './../styles/globals.css'
+// import AppHeader from "../components/AppHeader/AppHeader";
+import "./../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Flex
+        flexDirection="column"
+        gap="50px"
+        m="0 auto"
+        p="10px"
+        maxWidth="1100px"
+      >
+        {/* <AppHeader /> */}
+        <Flex as="main" gap="50px">
+          <Component {...pageProps} />
+        </Flex>
+      </Flex>
     </ChakraProvider>
   );
 }
